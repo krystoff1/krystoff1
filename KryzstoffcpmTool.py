302eab2f68cd550b18da1b3d5d4243c8d35995be
krystoff1 <krystofftermux@gmail.com>
2025-03-08 11:44:06 +0800

    Update KryzstoffcpmTool.py

--- KryzstoffcpmTool.py
+++ KryzstoffcpmTool.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from kryzstoffcpm import krystoff1
+from kryzstoffcpm import Krystoff1
 
 __CHANNEL_USERNAME__ = "XenoMainChannel"
 __GROUP_USERNAME__   = "XenoMainChat"
